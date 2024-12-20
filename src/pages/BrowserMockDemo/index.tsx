83265ebd63f3970a15e330649ae69eea55eb3063
emmanuelhashy <emmanuelhashy@gmail.com>
2024-12-20 08:25:57 +0100

    .

--- src/pages/BrowserMockDemo/index.tsx
+++ src/pages/BrowserMockDemo/index.tsx
@@ -2,7 +2,7 @@ import {Safari} from "../../components/browser-mock";
 
 export function SafariDemo() {
   return (
-    <div className="relative">
+    <div className="relative pb-20">
       <Safari url="livescore.com" className="size-full" />
     </div>
   );
